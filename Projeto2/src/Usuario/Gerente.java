d21d0674013e05bcce37f79f69834b9fd5a12895
Willian Murayama <willianyos13@gmail.com>
2022-12-27 16:20:42 -0300

    Implementação de gerente

--- Projeto2/src/Usuario/Gerente.java
+++ Projeto2/src/Usuario/Gerente.java
@@ -5,11 +5,32 @@
 package Usuario;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Calendar;
 
 /**
  *
  * @author Willian Murayama
  */
 public class Gerente extends Usuario implements Serializable {
+
+    protected float salario;
+    protected String pis;
+    protected Calendar dataAdmissao;
+
+    public Gerente(float salario, String pis, Calendar dataAdmissao, int codigo, String nome, String cpf, String rg, LocalDate dataNascimento, String endereco, String cep, String email) {
+        super(codigo, nome, cpf, rg, dataNascimento, endereco, cep, email);
+        this.salario = salario;
+        this.pis = pis;
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public String toString() {
+        return super.toString() + """
+               Salario: %.2f
+               PIS: %s
+               Data de admissao: %s
+               """.formatted(salario, pis, dataAdmissao.toString());
+    }
 
 }
