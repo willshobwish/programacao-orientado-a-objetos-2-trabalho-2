2a0f81b718bdb1ac54d90b891a31ab52c0d37720
Willian Murayama <willianyos13@gmail.com>
2022-12-28 19:43:03 -0300

    Correção do tipo de dado

--- Projeto2/src/Usuario/Gerente.java
+++ Projeto2/src/Usuario/Gerente.java
@@ -6,7 +6,7 @@ package Usuario;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Calendar;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
@@ -16,9 +16,9 @@ public class Gerente extends Usuario implements Serializable {
 
     protected float salario;
     protected String pis;
-    protected Calendar dataAdmissao;
+    protected LocalDate dataAdmissao;
 
-    public Gerente(float salario, String pis, Calendar dataAdmissao, int codigo, String nome, String cpf, String rg, LocalDate dataNascimento, String endereco, String cep, String email) {
+    public Gerente(float salario, String pis, LocalDate dataAdmissao, int codigo, String nome, String cpf, String rg, LocalDate dataNascimento, String endereco, String cep, String email) {
         super(codigo, nome, cpf, rg, dataNascimento, endereco, cep, email);
         this.salario = salario;
         this.pis = pis;
@@ -27,10 +27,10 @@ public class Gerente extends Usuario implements Serializable {
 
     public String toString() {
         return super.toString() + """
-               Salario: %.2f
+               Salario: R$ %.2f
                PIS: %s
                Data de admissao: %s
-               """.formatted(salario, pis, dataAdmissao.toString());
+               """.formatted(salario, pis, dataAdmissao.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")).toString());
     }
 
 }
