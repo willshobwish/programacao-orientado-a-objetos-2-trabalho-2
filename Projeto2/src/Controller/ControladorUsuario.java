6d25bc3b63b07a69d95c66d87c3b4a77b5530691
Willian Murayama <willianyos13@gmail.com>
2022-12-28 17:05:57 -0300

    Implementação de métodos

--- Projeto2/src/Controller/ControladorUsuario.java
+++ Projeto2/src/Controller/ControladorUsuario.java
@@ -4,10 +4,29 @@
  */
 package Controller;
 
+import Usuario.Cliente;
+import java.time.LocalDate;
+import java.util.Iterator;
+
 /**
  *
  * @author Willian Murayama
  */
 public class ControladorUsuario {
-    
+
+    public static void cadastrarCliente(int codigo, String nome, String cpf, String rg, LocalDate dataNascimento, String endereco, String cep, String email, LocalDate dataCadastro) {
+        Comercio.ComercioEletronico.cadastrarCliente(codigo, nome, cpf, rg, dataNascimento, endereco, cep, email, dataCadastro);
+    }
+
+    public static void cadastrarClienteOuro(int codigo, String nome, String cpf, String rg, LocalDate dataNascimento, String endereco, String cep, String email, LocalDate dataCadastro) {
+        Comercio.ComercioEletronico.cadastrarClienteOuro(codigo, nome, cpf, rg, dataNascimento, endereco, cep, email, dataCadastro);
+    }
+
+    public static Iterator<Cliente> iteratorTodosClientes() {
+        return Comercio.ComercioEletronico.getClientes().iterator();
+    }
+
+    public static int codigoCliente() {
+        return Comercio.ComercioEletronico.getClientes().size();
+    }
 }
