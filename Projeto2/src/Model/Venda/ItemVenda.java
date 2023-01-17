4561fd745ba536f81d9df42d3d569ac56701aff5
Bruno Augusto Furquim <97642457+BrunoAFurquim@users.noreply.github.com>
2023-01-17 00:22:02 -0300

    venda and itemVenda Model

--- Projeto2/src/Model/Venda/ItemVenda.java
+++ Projeto2/src/Model/Venda/ItemVenda.java
@@ -6,5 +6,41 @@ Giovanna Silva Custodio
 Willian Yoshio Murayama
  */
 public class ItemVenda {
+    private int coddigoProduto;
+    private float valor;
+    private int quantidade;
 
+    public void cadastrarItemVenda(int coddigoProduto, float valor, int quantidade) {
+        this.coddigoProduto = coddigoProduto;
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    public int getCoddigoProduto() {
+        return coddigoProduto;
+    }
+
+    public void setCoddigoProduto(int coddigoProduto) {
+        this.coddigoProduto = coddigoProduto;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float calcularTotal() {
+        return valor * quantidade;
+    }
 }
