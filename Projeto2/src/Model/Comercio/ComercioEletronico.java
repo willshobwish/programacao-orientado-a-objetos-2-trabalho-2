ea06dae3f267b6e2b2779a52acb0c6efb661a43a
Willian Murayama <willianyos13@gmail.com>
2023-01-20 11:03:56 -0300

    Correção da implementação de cliente

--- Projeto2/src/Model/Comercio/ComercioEletronico.java
+++ Projeto2/src/Model/Comercio/ComercioEletronico.java
@@ -4,6 +4,7 @@ import Model.Fabricante.Fabricante;
 import Model.Produto.Produto;
 import Model.Transportadora.Transportadora;
 import Model.Usuario.Cliente;
+import Model.Usuario.ClienteNormal;
 import Model.Usuario.ClienteOuro;
 import Model.Usuario.Gerente;
 import Model.Venda.Venda;
@@ -16,67 +17,66 @@ Giovanna Silva Custodio
 Willian Yoshio Murayama
  */
 public class ComercioEletronico {
-    
+
     private static ArrayList<Venda> vendas = Controller.ControladorSerializable.carregarVendas();
     private static ArrayList<Produto> produtos = Controller.ControladorSerializable.carregarProdutos();
     private static ArrayList<Fabricante> fabricantes = Controller.ControladorSerializable.carregarFabricantes();
     private static ArrayList<Transportadora> transportadoras = Controller.ControladorSerializable.carregarTransportadoras();
     private static ArrayList<Cliente> clientes = Controller.ControladorSerializable.carregarClientes();
-    private static ArrayList<ClienteOuro> clientesOuro = Controller.ControladorSerializable.carregarClientesOuro();
+//    private static ArrayList<ClienteOuro> clientesOuro = Controller.ControladorSerializable.carregarClientesOuro();
     private static ArrayList<Gerente> gerentes = Controller.ControladorSerializable.carregarGerentes();
-    
+
     public ComercioEletronico() {
     }
-    
-    public static void cadastrarCliente(int codigo, String nome, String cpf, String rg, LocalDate dataNascimento, String endereco, String cep, String email, LocalDate dataCadastro) {
-        Cliente clienteCadastro = new Cliente(codigo, nome, cpf, rg, dataNascimento, endereco, cep, email, dataCadastro);
+
+    public static void cadastrarClienteNormal(int codigo, String nome, String cpf, String rg, LocalDate dataNascimento, String endereco, String cep, String email, LocalDate dataCadastro) {
+        Cliente clienteCadastro = new ClienteNormal(codigo, nome, cpf, rg, dataNascimento, endereco, cep, email, dataCadastro);
         clientes.add(clienteCadastro);
         Controller.ControladorSerializable.salvarClientes(clientes);
     }
-    
+
     public static void cadastrarClienteOuro(int codigo, String nome, String cpf, String rg, LocalDate dataNascimento, String endereco, String cep, String email, LocalDate dataCadastro) {
         ClienteOuro clienteCadastro = new ClienteOuro(codigo, nome, cpf, rg, dataNascimento, endereco, cep, email, dataCadastro);
-        clientesOuro.add(clienteCadastro);
-        Controller.ControladorSerializable.salvarClientesOuro(clientesOuro);
+        clientes.add(clienteCadastro);
+        Controller.ControladorSerializable.salvarClientes(clientes);
     }
-    
+
     public static void cadastrarGerente(float salario, String pis, LocalDate dataAdmissao, int codigo, String nome, String cpf, String rg, LocalDate dataNascimento, String endereco, String cep, String email) {
         Gerente gerenteCadastro = new Gerente(salario, pis, dataAdmissao, codigo, nome, cpf, rg, dataNascimento, endereco, cep, email);
         gerentes.add(gerenteCadastro);
         Controller.ControladorSerializable.salvarGerentes(gerentes);
     }
-    
+
     public static void cadastrarProduto(Produto Produto) {
         produtos.add(Produto);
         Controller.ControladorSerializable.salvarProdutos(produtos);
     }
-    
+
     public static ArrayList<Venda> getVendas() {
         return vendas;
     }
-    
+
     public static ArrayList<Produto> getProdutos() {
         return produtos;
     }
-    
+
     public static ArrayList<Fabricante> getFabricantes() {
         return fabricantes;
     }
-    
+
     public static ArrayList<Transportadora> getTransportadoras() {
         return transportadoras;
     }
-    
+
     public static ArrayList<Cliente> getClientes() {
         return clientes;
     }
-    
-    public static ArrayList<ClienteOuro> getClientesOuro() {
-        return clientesOuro;
-    }
-    
+
+//    public static ArrayList<ClienteOuro> getClientesOuro() {
+//        return clientesOuro;
+//    }
     public static ArrayList<Gerente> getGerentes() {
         return gerentes;
     }
-    
+
 }
--- Projeto2/src/Model/Usuario/ClienteOuro.java
+++ Projeto2/src/Model/Usuario/ClienteOuro.java
@@ -1,5 +1,6 @@
 package Model.Usuario;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 /*
@@ -7,10 +8,11 @@ Bruno Augusto Furquim
 Giovanna Silva Custodio
 Willian Yoshio Murayama
  */
-public class ClienteOuro extends Cliente {
+public class ClienteOuro extends Cliente implements Serializable {
 
     public ClienteOuro(int codigo, String nome, String cpf, String rg, LocalDate dataNascimento, String endereco, String cep, String email, LocalDate dataCadastro) {
         super(codigo, nome, cpf, rg, dataNascimento, endereco, cep, email, dataCadastro);
+
     }
 
     public String toString() {
