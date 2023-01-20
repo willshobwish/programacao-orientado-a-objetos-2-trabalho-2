b4bd888e9b100bbf8690847446690680aa2df204
Bruno Augusto Furquim <97642457+BrunoAFurquim@users.noreply.github.com>
2023-01-20 14:40:53 -0300

    Update ControladorTransportadora
    nova implementação do método topTransportadora() utilizando treeMap

--- Projeto2/src/Controller/ControladorTransportadora.java
+++ Projeto2/src/Controller/ControladorTransportadora.java
@@ -1,7 +1,11 @@
 package Controller;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 import Model.Transportadora.Transportadora;
 import Model.Venda.Venda;
@@ -23,36 +27,88 @@ public class ControladorTransportadora {
         return transportadoras;
     }
 
-    public static ArrayList<Transportadora> topTransportadoras() {
-
-        Transportadora transportadora1 = new Transportadora();
-        Transportadora transportadora2 = new Transportadora();
-        Transportadora transportadora3 = new Transportadora();
-        ArrayList<Transportadora> topTransportadoras = new ArrayList<Transportadora>();
-
+    public static ArrayList<Model.Transportadora.Transportadora> topTransportadoras() {
+        Map<Transportadora, Integer> map = new TreeMap<>();
+        ArrayList<Transportadora> result = new ArrayList<>();
+        ArrayList<Transportadora> aux = new ArrayList<>();
         Iterator transportadorasCadastradas = Model.Comercio.ComercioEletronico.getTransportadoras().iterator();
         Iterator vendas = Model.Comercio.ComercioEletronico.getVendas().iterator();
 
         while (vendas.hasNext()) {
             Venda venda = (Venda) vendas.next();
             Transportadora transportadora = (Transportadora) transportadorasCadastradas.next();
+            map.put(transportadora, venda.vendasTransportadoras(transportadora));
+        }
 
-            if (venda.vendasTransportadoras(transportadora) > venda.vendasTransportadoras(transportadora1)) {
-                transportadora3 = transportadora2;
-                transportadora2 = transportadora1;
-                transportadora1 = transportadora;
-            } else if (venda.vendasTransportadoras(transportadora) > venda.vendasTransportadoras(transportadora2)) {
-                transportadora3 = transportadora2;
-                transportadora2 = transportadora;
-            } else if (venda.vendasTransportadoras(transportadora) > venda.vendasTransportadoras(transportadora3)) {
-                transportadora3 = transportadora;
+        map = ordenarValor(map);
+        map.forEach((k, v) -> aux.add(k));
+        int count = 0;
+
+        for (Transportadora t : aux) {
+            result.add(t);
+            count++;
+            if (count == 3) {
+                break;
             }
         }
 
-        topTransportadoras.add(transportadora1);
-        topTransportadoras.add(transportadora2);
-        topTransportadoras.add(transportadora3);
+        return result;
 
-        return topTransportadoras;
     }
+
+    public static <Transportadora, Integer extends Comparable<Integer>> Map<Transportadora, Integer> ordenarValor(
+            final Map<Transportadora, Integer> map) {
+        Comparator<Transportadora> valueComparator = new Comparator<Transportadora>() {
+            public int compare(Transportadora k1, Transportadora k2) {
+                int compare = map.get(k1).compareTo(map.get(k2));
+                if (compare == 0)
+                    return 1;
+                else
+                    return compare;
+            }
+        };
+        Map<Transportadora, Integer> sortedByValues = new TreeMap<Transportadora, Integer>(valueComparator);
+        sortedByValues.putAll(map);
+
+        return sortedByValues;
+    }
+
+    // public static ArrayList<Transportadora> topTransportadoras() {
+
+    // Transportadora transportadora1 = new Transportadora();
+    // Transportadora transportadora2 = new Transportadora();
+    // Transportadora transportadora3 = new Transportadora();
+    // ArrayList<Transportadora> topTransportadoras = new
+    // ArrayList<Transportadora>();
+
+    // Iterator transportadorasCadastradas =
+    // Model.Comercio.ComercioEletronico.getTransportadoras().iterator();
+    // Iterator vendas = Model.Comercio.ComercioEletronico.getVendas().iterator();
+
+    // while (vendas.hasNext()) {
+    // Venda venda = (Venda) vendas.next();
+    // Transportadora transportadora = (Transportadora)
+    // transportadorasCadastradas.next();
+
+    // if (venda.vendasTransportadoras(transportadora) >
+    // venda.vendasTransportadoras(transportadora1)) {
+    // transportadora3 = transportadora2;
+    // transportadora2 = transportadora1;
+    // transportadora1 = transportadora;
+    // } else if (venda.vendasTransportadoras(transportadora) >
+    // venda.vendasTransportadoras(transportadora2)) {
+    // transportadora3 = transportadora2;
+    // transportadora2 = transportadora;
+    // } else if (venda.vendasTransportadoras(transportadora) >
+    // venda.vendasTransportadoras(transportadora3)) {
+    // transportadora3 = transportadora;
+    // }
+    // }
+
+    // topTransportadoras.add(transportadora1);
+    // topTransportadoras.add(transportadora2);
+    // topTransportadoras.add(transportadora3);
+
+    // return topTransportadoras;
+    // }
 }
