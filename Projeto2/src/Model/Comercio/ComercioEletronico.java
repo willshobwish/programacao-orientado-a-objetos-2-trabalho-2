0af763d916961be154452c0eec5f9822e96a13ce
Willian Murayama <willianyos13@gmail.com>
2023-01-20 17:43:05 -0300

    Implementação de métodos
    Implementação do cadastro de transportadora  e cadastro de venda

--- Projeto2/src/Model/Comercio/ComercioEletronico.java
+++ Projeto2/src/Model/Comercio/ComercioEletronico.java
@@ -1,12 +1,14 @@
 package Model.Comercio;
 
 import Model.Fabricante.Fabricante;
+import Model.Pagamento.Pagamento;
 import Model.Produto.Produto;
 import Model.Transportadora.Transportadora;
 import Model.Usuario.Cliente;
 import Model.Usuario.ClienteNormal;
 import Model.Usuario.ClienteOuro;
 import Model.Usuario.Gerente;
+import Model.Venda.ItemVenda;
 import Model.Venda.Venda;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -17,7 +19,7 @@ Giovanna Silva Custodio
 Willian Yoshio Murayama
  */
 public class ComercioEletronico {
-
+    
     private static ArrayList<Venda> vendas = Controller.ControladorSerializable.carregarVendas();
     private static ArrayList<Produto> produtos = Controller.ControladorSerializable.carregarProdutos();
     private static ArrayList<Fabricante> fabricantes = Controller.ControladorSerializable.carregarFabricantes();
@@ -25,49 +27,60 @@ public class ComercioEletronico {
     private static ArrayList<Cliente> clientes = Controller.ControladorSerializable.carregarClientes();
 //    private static ArrayList<ClienteOuro> clientesOuro = Controller.ControladorSerializable.carregarClientesOuro();
     private static ArrayList<Gerente> gerentes = Controller.ControladorSerializable.carregarGerentes();
-
+    
     public ComercioEletronico() {
     }
-
+    
     public static void cadastrarClienteNormal(int codigo, String nome, String cpf, String rg, LocalDate dataNascimento, String endereco, String cep, String email, LocalDate dataCadastro) {
         Cliente clienteCadastro = new ClienteNormal(codigo, nome, cpf, rg, dataNascimento, endereco, cep, email, dataCadastro);
         clientes.add(clienteCadastro);
         Controller.ControladorSerializable.salvarClientes(clientes);
     }
-
+    
     public static void cadastrarClienteOuro(int codigo, String nome, String cpf, String rg, LocalDate dataNascimento, String endereco, String cep, String email, LocalDate dataCadastro) {
         ClienteOuro clienteCadastro = new ClienteOuro(codigo, nome, cpf, rg, dataNascimento, endereco, cep, email, dataCadastro);
         clientes.add(clienteCadastro);
         Controller.ControladorSerializable.salvarClientes(clientes);
     }
-
+    
     public static void cadastrarGerente(float salario, String pis, LocalDate dataAdmissao, int codigo, String nome, String cpf, String rg, LocalDate dataNascimento, String endereco, String cep, String email) {
         Gerente gerenteCadastro = new Gerente(salario, pis, dataAdmissao, codigo, nome, cpf, rg, dataNascimento, endereco, cep, email);
         gerentes.add(gerenteCadastro);
         Controller.ControladorSerializable.salvarGerentes(gerentes);
     }
-
+    
     public static void cadastrarProduto(Produto Produto) {
         produtos.add(Produto);
         Controller.ControladorSerializable.salvarProdutos(produtos);
     }
-
+    
+    public static void cadastrarTransportadora(int codigo, String cnpj, String nome, String email, String telefone, String endereco, int tempoDeEntrega) {
+        transportadoras.add(new Transportadora(codigo, cnpj, nome, email, telefone, endereco, tempoDeEntrega));
+        Controller.ControladorSerializable.salvarTransportadoras(transportadoras);
+        
+    }
+    
+    public static void cadastrarVenda(int codigo, Cliente cliente, Gerente gerente, LocalDate dataVenda, LocalDate dataDaEntrega, ArrayList<ItemVenda> itensVenda, float valorTotal, float valorComDesconto, Pagamento formaPagamento, Transportadora transportadora) {
+        vendas.add(new Venda(codigo, cliente, gerente, dataVenda, dataDaEntrega, itensVenda, valorTotal, valorComDesconto, formaPagamento, transportadora));
+        Controller.ControladorSerializable.salvarVendas(vendas);
+    }
+    
     public static ArrayList<Venda> getVendas() {
         return vendas;
     }
-
+    
     public static ArrayList<Produto> getProdutos() {
         return produtos;
     }
-
+    
     public static ArrayList<Fabricante> getFabricantes() {
         return fabricantes;
     }
-
+    
     public static ArrayList<Transportadora> getTransportadoras() {
         return transportadoras;
     }
-
+    
     public static ArrayList<Cliente> getClientes() {
         return clientes;
     }
@@ -78,5 +91,5 @@ public class ComercioEletronico {
     public static ArrayList<Gerente> getGerentes() {
         return gerentes;
     }
-
+    
 }
