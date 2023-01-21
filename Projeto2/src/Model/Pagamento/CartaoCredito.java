f4b048898b92be275337dc0059872f9657f813ee
Willian Murayama <willianyos13@gmail.com>
2023-01-21 11:18:52 -0300

    Correção do construtor

--- Projeto2/src/Model/Pagamento/CartaoCredito.java
+++ Projeto2/src/Model/Pagamento/CartaoCredito.java
@@ -11,10 +11,6 @@ public class CartaoCredito extends Pagamento {
     private String bandeira;
     private String numero;
 
-    public CartaoCredito(int codigoNota) {
-        super(codigoNota);
-    }
-
     public CartaoCredito(String nome, String bandeira, String numero, int codigoNota) {
         super(codigoNota);
         this.nome = nome;
